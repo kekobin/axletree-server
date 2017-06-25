08739255cc2772520e7efd46cfebe6ca4b249820
kebin <kekobin@163.com>
2017-06-25 16:26:15 +0800

    init.

---
+++ config/apiConf.js
@@ -0,0 +1,16 @@
+module.exports = {
+	"axletree-demo": {
+		"pages": [
+			{
+				"page": "index",
+				"urls": [
+					{
+						"name": "api1",
+						"url":"http://www.huya.com/hd/olddriver2/index.php?m=OldDriver2&do=ajaxGetCombatInfo"
+					}
+				],
+				"cache": true
+			}
+		]
+	}
+}
